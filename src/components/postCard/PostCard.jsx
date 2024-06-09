f85a35abe5c60ac1c990389851223880292d239e
Anurag <it.anuragadhikari>
2024-06-09 16:24:38 +0530

    postcard jsx updated

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import styles from "./postCard.module.css"
 import Link from "next/link"
 
-const PostCard = () => {
+const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
 
@@ -11,13 +11,14 @@ const PostCard = () => {
           <Image src="https://images.pexels.com/photos/10305838/pexels-photo-10305838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
         </div>
         <span className={styles.date}>01.01.2024</span>
+        
       </div>
 
 
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia iste voluptatem assumenda sint perferendis totam distinctio sit debitis eligendi tempora.</p>
-        <Link className={styles.link} href="/blog/post">Read More</Link>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link className={styles.link} href={`/blog/${post.id}`}>READ MORE</Link>
       </div>
     </div>
   )
